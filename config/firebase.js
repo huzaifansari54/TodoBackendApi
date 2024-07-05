cebe47bcf178ac4fbaceedb021e8c82e67e729fa
Huzaif ansari <78638431+huzaifansari54@users.noreply.github.com>
2024-07-05 21:35:24 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,13 @@ require("firebase/firestore");
 
 // Add Firebase SDK Snippet
 const firebaseConfig = {
-  apiKey: "AIzaSyB1-gOo9bumveVGrUSR0QgQ8wwqlMB6z6c",
-  authDomain: "todo-app-533f3.firebaseapp.com",
-  projectId: "todo-app-533f3",
-  storageBucket: "todo-app-533f3.appspot.com",
-  messagingSenderId: "810387081435",
-  appId: "1:810387081435:web:fe41e0de2c972cf3a3a4fe",
-  measurementId: "G-125FQKE2SX"
+  apiKey: "ur api key",
+  authDomain: "m",
+  projectId: "t3",
+  storageBucket: "todm",
+  messagingSenderId: "81",
+  appId: "1:8104fe",
+  measurementId: "SX"
 };
 
 firebase.initializeApp(firebaseConfig);
